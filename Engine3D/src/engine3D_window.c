960b1e4ba57d37e4aa804588beb744909f6d9c95
David <donpolilla@gmail.com>
2018-10-08 00:59:00 +0200

    Fixed for linux

--- Engine3D/src/engine3D_window.c
+++ Engine3D/src/engine3D_window.c
@@ -14,6 +14,8 @@ static const char *current_title;
 
 // typedef void (GLAPIENTRY *GLDEBUGPROC)(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam);
 static void GLAPIENTRY debugMessage(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *message, const void *userParam) {
+	(void)userParam;
+
 	switch (source) {
 	case GL_DEBUG_SOURCE_API:
 		printf("[API]");
